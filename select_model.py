7dd69e08657453d5d8fac012126632a86bb8e62c
plablo09 <pablo.lopez@centrogeo.edu.mx>
2016-09-13 19:47:40 -0500

    First model fitting workflow.

--- select_model.py
+++ select_model.py
@@ -40,11 +40,35 @@ X, Y = data_X[data_Y != 2], data_Y[data_Y != 2]
 X, Y = X[Y != 4], Y[Y != 4]
 # recode class:
 Y[Y==3] = 0
-# Scale sample
-scaled_X = preprocessing.scale(X)
+
 # test and train split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y,
+                                                    test_size=0.4,
+                                                    random_state=random_state)
+# Scale sample
+scaled_X = preprocessing.scale(X_train)
 
+# parameters for model fitting
+param_grid = {'C': [1, 10, 100, 1000], 'gamma': [0.01,0.001, 0.0001],
+              'kernel': ['rbf']}
+metrics = ['f1','accuracy','average_precision','roc_auc','recall']
 
-param_grid = {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['rbf']}
-clf = fit_model(scaled_X,Y,param_grid,'f1',6)
-clf.best_score_
+# Fit models and store them
+fitted_models = {}
+for metric in metrics:
+    fitted_models[metric] = fit_model(scaled_X,Y_train,param_grid,metric,6)
+
+for metric, model in fitted_models.items():
+    print ("Using metric {}".format(metric))
+    print("Best parameters set found on development set:")
+    print()
+    print(model.best_params_)
+    print("Grid scores on development set:")
+    print()
+    for params, mean_score, scores in model.grid_scores_:
+        print("%0.3f (+/-%0.03f) for %r"
+              % (mean_score, scores.std() * 2, params))
+
+    print()
+
+#
